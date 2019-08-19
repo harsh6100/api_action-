42bf98e921150986ec118fb2b38f8a51265713b5
harsh6100 <52690817+harsh6100@users.noreply.github.com>
2019-08-20 00:52:00 +0530

    Update book_post.py

--- actions/book_post.py
+++ actions/book_post.py
@@ -8,9 +8,15 @@ from st2common.runners.base_action import Action
 
 class MyAction(Action):
 	def run(self,id,title,descp,pgcount,excerpt,pubdate):
-		resp=requests.post('https://fakerestapi.azurewebsites.net/api/Books',data={"ID": id,"Title": title,"Description": descp,"PageCount": pgcount,"Excerpt": excerpt,  "PublishDate": pubdate})
-	        data=resp.json()
-		print(data)
+        data={"ID": id,"Title":title,"Description": descp,"PageCount": pgcount,"Excerpt": excerpt,  "PublishDate": pubdate}
+        data1=json.dumps(data)
+        headers={'content-type': 'application/json'}
+        url='https://fakerestapi.azurewebsites.net/api/Books'
+        response=requests.post(url,headers=headers,data=data1)
+        print(response)
+        data2=response.json()
+        print(data2)
+
 		
 
 
