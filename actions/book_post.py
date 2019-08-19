8d72d6e8e411a26fcdf6c717dff3b21303c4e993
harsh <harsh6100@gmail.com>
2019-08-19 18:06:13 +0000

    On branch master

--- actions/book_post.py
+++ actions/book_post.py
@@ -7,7 +7,7 @@ from st2common.runner.base_action import Action
 
 class MyAction(Action):
 	def run(self,id,title,descp,pgcount,excerpt,pubdate):
-		resp=requests.post(https://fakerestapi.azurewebsites.net/api/Books,data={"ID": id,"Title": title,"Description": descp,"PageCount": pgcount,"Excerpt": excerpt,  "PublishDate": pubdate})
+		resp=requests.post('https://fakerestapi.azurewebsites.net/api/Books',data={"ID": id,"Title": title,"Description": descp,"PageCount": pgcount,"Excerpt": excerpt,  "PublishDate": pubdate})
 	        print(resp.title)
 
 
