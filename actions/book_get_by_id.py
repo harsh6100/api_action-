f504bef28b8869a3a6d07ae4ea05d3c6effa74cf
harsh6100 <52690817+harsh6100@users.noreply.github.com>
2019-08-20 20:59:34 +0530

    Update book_get_by_id.py

--- actions/book_get_by_id.py
+++ actions/book_get_by_id.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python2
 
+import sys
+import requests 
+import json
+
+from st2common.runners.base_action import Action
+
+class MyEchoAction(Action):
+    def run(self, id):        
+        headers={'content-type': 'application/json'}
+        url='https://fakerestapi.azurewebsites.net/api/Books/'+id
+        response=requests.get(url,headers=headers,timeout=6.0)
+        print(response)
+        data2=response.json()
+        print(data2)
+
+
+          
