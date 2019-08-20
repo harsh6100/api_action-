6409cf579a2278a0056e1fe31dbe8340ce442d25
harsh6100 <52690817+harsh6100@users.noreply.github.com>
2019-08-20 14:51:13 +0530

    Update demo_api.py

--- actions/demo_api.py
+++ actions/demo_api.py
@@ -11,7 +11,5 @@ class MyEchoAction(Action):
 		resp = req.get(url)
 		print(resp.status_code)
 		print(resp.url)
-	except requests.exceptions.RequestException:
-    		print ("Url cant be reached")
-    		sys.exit(1)
+	
 	
