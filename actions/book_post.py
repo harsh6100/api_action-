024d2ad315a2e35d11310487d45fcbeaa38ba6fc
harsh6100 <52690817+harsh6100@users.noreply.github.com>
2019-08-20 20:12:27 +0530

    Update book_post.py

--- actions/book_post.py
+++ actions/book_post.py
@@ -8,14 +8,19 @@ from st2common.runners.base_action import Action
 
 class MyAction(Action):
 	def run(self,id,title,descp,pgcount,excerpt,pubdate):
-		data={"ID": id,"Title":title,"Description": descp,"PageCount": pgcount,"Excerpt": excerpt,  "PublishDate": pubdate}
-		data1=json.dumps(data)
-		headers={'content-type': 'application/json'}
-		url='https://fakerestapi.azurewebsites.net/api/Books'
-		response=requests.post(url,headers=headers,data=data1)
-		print(response)
-		data2=response.json()
-		print(data2)
+		try:
+			data={"ID": id,"Title":title,"Description": descp,"PageCount": pgcount,"Excerpt": excerpt,  "PublishDate": pubdate}
+			data1=json.dumps(data)
+			headers={'content-type': 'application/json'}
+			url='https://fakerestapi.azurewebsites.net/api/Books'
+			response=requests.post(url,headers=headers,data=data1)
+			print(response)
+			data2=response.json()
+			print(data2)
+			
+		except req.exceptions.Timeout:
+                        print("Request timeout")
+                        sys.exit(0)
 
 		
 
