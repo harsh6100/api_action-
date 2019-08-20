d2cf9fad75fc75da01fd81a57cea2f20222390d2
harsh6100 <52690817+harsh6100@users.noreply.github.com>
2019-08-20 12:54:23 +0530

    Update demo_api.py

--- actions/demo_api.py
+++ actions/demo_api.py
@@ -11,9 +11,9 @@ class MyEchoAction(Action):
 		resp = req.get(url)
 		print(resp.status_code)
 		print(resp.url)
-	except requests.exceptions.RequestException:
+	except requests.exceptions.RequestException as e:
     		print (e)
     		sys.exit(1)
-	except NameError as e
+	except NameError as error:
 		print ("invalid URL")
 		sys.exit(1)
