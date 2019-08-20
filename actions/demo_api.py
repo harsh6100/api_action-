637bd2c3b56d28fc0c5e74ca97c7759b5caf735d
harsh6100 <52690817+harsh6100@users.noreply.github.com>
2019-08-20 12:30:08 +0530

    Update demo_api.py

--- actions/demo_api.py
+++ actions/demo_api.py
@@ -7,6 +7,10 @@ from st2common.runners.base_action import Action
 
 class MyEchoAction(Action):
     def run(self, url):
+	try:
 		resp = req.get(url)
 		print(resp.status_code)
 		print(resp.url)
+	except requests.exceptions.RequestException as e:
+    		print e
+    		sys.exit(1)
