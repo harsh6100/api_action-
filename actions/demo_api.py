f0af9ac8a690bcc1d05291c78b2f5a71bb29db2d
harsh6100 <52690817+harsh6100@users.noreply.github.com>
2019-08-20 19:53:11 +0530

    Update demo_api.py

--- actions/demo_api.py
+++ actions/demo_api.py
@@ -8,10 +8,12 @@ from st2common.runners.base_action import Action
 class MyEchoAction(Action):
     def run(self, url):
 	try:
-		resp = req.get(url)
+		resp = req.get(url,timeout=6.0)
 		print(resp.status_code)
 		print(resp.url)
 	except req.exceptions.MissingSchema:
                 print("invalid URL")
                 sys.exit(0)
-	
+	except req.exceptions.Timeout:
+                print("Request timeout")
+                sys.exit(0)
