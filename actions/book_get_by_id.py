8bbfb5a60fd009b34ea77b4c045e3ff9d89fa98a
harsh6100 <52690817+harsh6100@users.noreply.github.com>
2019-08-22 09:46:34 +0530

    Update book_get_by_id.py

--- actions/book_get_by_id.py
+++ actions/book_get_by_id.py
@@ -7,14 +7,19 @@ import json
 from st2common.runners.base_action import Action
 
 class MyEchoAction(Action):
-    def run(self, id):       
-        val=str(id)
-        headers={'content-type': 'application/json'}
-        url='https://fakerestapi.azurewebsites.net/api/Books/'+val
-        response=requests.get(url,headers=headers,timeout=6.0)
-        print(response)
-        data2=response.json()
-        print(data2)
+    def run(self, id):
+        try:
+            val=str(id)
+            headers={'content-type': 'application/json'}
+            url='https://fakerestapi.azurewebsites.net/api/Books/'+val
+            response=requests.get(url,headers=headers,timeout=6.0)
+            print(response)
+            data2=response.json()
+            print(data2)
+        except requests.exceptions.Timeout:
+            print("Request timeout")
+            sys.exit(0)
+            
 
 
           
